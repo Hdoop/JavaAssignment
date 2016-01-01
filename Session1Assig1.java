3e2fcdc86666ea597da1ec0f5e5cb650353175ce
ravinder singh <ravinder@atjam.com>
2016-01-01 15:50:03 +0530

    //Write a program to print the sum of two numbers. One number should be int and another should be float.
    Done

---
+++ Session1Assig1.java
@@ -0,0 +1,28 @@
+package com.assignment.example;
+
+public class Session1Assig1 {
+
+	public Session1Assig1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Write a program to print the sum of two numbers. One number should be int and another should be float.
+		
+		int _int =10 ;float _flt =300.98f;
+		
+		int result =_int+(int)_flt;
+		
+		//Sum of two variable with explicit conversion...
+		System.out.println(result);
+		
+		
+		//Sum of variables int and float saved back to float 
+		float fltresult =_int+_flt ;
+		
+		System.out.println(fltresult);
+		
+	}
+
+}
