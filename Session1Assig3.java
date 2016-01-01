51c1b217e121a0efac6b3f7be957ae4d9412a013
ravinder singh <ravinder@atjam.com>
2016-01-01 15:39:21 +0530

    //Declare three integer variables (a,b,c) in a single declaration and simultaneously initialize b to 4.
    Done

---
+++ Session1Assig3.java
@@ -0,0 +1,18 @@
+package com.assignment.example;
+
+public class Session1Assig3 {
+
+	public Session1Assig3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Declare three integer variables (a,b,c) in a single declaration and simultaneously initialize b to 4.
+		int a,b=4,c;
+		a =1;
+		c=11;
+		System.out.printf("variable a is %d b is %d and c %d\n",a,b,c);
+	}
+
+}
